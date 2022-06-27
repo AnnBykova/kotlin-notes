b1e63e20f3a8a322a8d36229c8ebc786dd364956
AnnBykova <ann_karelia>
2022-06-27 15:32:39 +0300

    Bugs fixed and tests added

---
+++ src/test/kotlin/NotesServiceTest.kt
@@ -0,0 +1,58 @@
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class NotesServiceTest {
+
+    @Test
+    fun getNotes() {
+    }
+
+    @Test
+    fun setNotes() {
+    }
+
+    @Test
+    fun setId() {
+    }
+
+    @Test
+    fun add() {
+    }
+
+    @Test
+    fun delete() {
+    }
+
+    @Test
+    fun getById() {
+    }
+
+    @Test
+    fun edit() {
+    }
+
+    @Test
+    fun get() {
+    }
+
+    @Test
+    fun setCommentsId() {
+    }
+
+    @Test
+    fun createComment() {
+    }
+
+    @Test
+    fun deleteComment() {
+    }
+
+    @Test
+    fun editComment() {
+    }
+
+    @Test
+    fun restoreComment() {
+    }
+}
